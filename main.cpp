0723cc9760e498f1e1a8a23ace20addaef66d8c8
daniyalrai364@gmail.com <daniyalrai364@gmail.com>
2026-05-13 21:16:11 +0100

    parser v1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+#include <unordered_map>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
 using namespace std;
+
+enum ArgumentType {
+    ADDRESS,
+    REGISTER,
+    IMMEDIATE
+};
+
+struct Argument {
+    ArgumentType type;
+    int value;
+};
 
 enum Opcode {
     LOAD,
@@ -14,5 +30,96 @@ enum Opcode {
 };
 
 struct Instruction {
+    Opcode op;
+    vector<Argument> args;
+};
+
+struct Branch {
+    string name;
+    int position;
+};
+
+unordered_map<int, char> symbolMap = {
+    {0, ' '},
+    {1, '|'},
+    {2, 'O'}
+};
+
+unordered_map<string, Opcode> opcodeMap = {
+    {"LOAD", LOAD},
+    {"STORE", STORE},
+    {"ADD", ADD},
+    {"SUB", SUB},
+    {"BEQ", BEQ},
+    {"BGT", BGT},
+    {"BLT", BLT},
+    {"B", B}
+};
+
+char mapChar(int value) {
+    return symbolMap[value];
+};
+
+Opcode mapOpcode(const string& token) {
+    return opcodeMap[token];
+};
+
+Argument parseArgument(const string& token) {
+    Argument arg;
     
+    ArgumentType type;
+    string cleanToken = token;
+
+    switch (token[0]) {
+        case 'R':
+            type = REGISTER;
+            cleanToken = token.substr(1);
+            break;
+
+        case '#':
+            type = IMMEDIATE;
+            cleanToken = token.substr(1);
+            break;
+
+        default:
+            type = ADDRESS;
+            break;
+    }
+
+    int value = stoi(cleanToken);
+
+    arg.type = type;
+    arg.value = value;
+
+    return arg;
+};
+
+Instruction parseLine(const string& line) {
+    Instruction result;
+    
+    istringstream iss(line);
+    string token;
+    iss >> token;
+
+    result.op = mapOpcode(token);
+
+    while (iss >> token) {
+        result.args.push_back(parseArgument(token));
+    };
+};
+
+void main() {
+    string fileName;
+
+    cout << "Enter file name: ";
+    cin >> fileName;
+
+    vector<Instruction> instructions;
+
+    ifstream programFile(fileName);
+
+    string programLine;
+    while (getline(programFile, programLine)) {
+        instructions.push_back(parseLine(programLine));
+    }
 };
